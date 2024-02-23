298897e6fac0225a6992e6e4a841f952ff1b9949
Eric Knorr <50336131+ia-eknorr@users.noreply.github.com>
2024-02-22 16:37:57 -0800

    Eknorr/add naming styles (#11)
    * chore: add .gitignore for .vscode
    
    * feat: add new style params to action
    
    * feat: add checker module for styles
    
    * feat: integrate style checker into main script
    
    * chore: add defaults to arguments
    
    * chore: rename py file
    
    * chore: fix test file
    
    * chore: remove class deafults
    
    * chore: remove default from argparse
    
    * chore: fix conditions for rgx params
    
    * chore: update tests for accuracy
    
    * feature: update for null case
    
    * chore: minor fix to add hyphens
    
    * chore: remove debug prints

---
+++ src/checker.py
@@ -0,0 +1,53 @@
+import re
+
+class StyleChecker:
+    """Class for defining different naming styles and checking names against them."""
+
+    @staticmethod
+    def is_snake_case(name: str) -> bool:
+        """Check if the name follows snake_case convention."""
+        return bool(re.match(r"^[a-z][a-z0-9_]*$", name))
+
+    @staticmethod
+    def is_camel_case(name: str) -> bool:
+        """Check if the name follows camelCase convention."""
+        return bool(re.match(r"^[a-z]+(?:[A-Z][a-z]*)*$", name))
+
+    @staticmethod
+    def is_pascal_case(name: str) -> bool:
+        """Check if the name follows PascalCase convention."""
+        return bool(re.match(r"^[A-Z][a-zA-Z0-9]*$", name))
+
+    @staticmethod
+    def is_upper_case(name: str) -> bool:
+        """Check if the name follows UPPER_CASE convention."""
+        return bool(re.match(r"^[A-Z0-9_]+$", name))
+
+    @staticmethod
+    def is_any(name: str) -> bool:
+        """Any name is considered correct."""
+        return True
+
+    def __init__(self, style_name):
+        self.style_name = style_name
+        self.style_check_function = self._get_style_check_function(style_name)
+
+    def _get_style_check_function(self, style_name):
+        naming_styles = {
+            "snake_case": self.is_snake_case,
+            "camelCase": self.is_camel_case,
+            "PascalCase": self.is_pascal_case,
+            "UPPER_CASE": self.is_upper_case,
+            "any": self.is_any,
+        }
+        if style_name in naming_styles:
+            return naming_styles[style_name]
+        else:
+            return self._generate_regex_check_function(self.style_name)
+
+    def _generate_regex_check_function(self, pattern):
+        regex_pattern = re.compile(pattern)
+        return lambda name: bool(regex_pattern.match(name))
+
+    def is_correct_style(self, name):
+        return self.style_check_function(name)
---
+++ src/ignition-lint.py
@@ -0,0 +1,124 @@
+import json
+import sys
+import argparse
+from checker import StyleChecker
+
+class JsonLinter:
+    def __init__(self, component_style, parameter_style, component_style_rgx, parameter_style_rgx):
+        # Check if both named style and regex style are provided for the same type
+        if component_style_rgx is not None and component_style is not None :
+            raise ValueError("Cannot specify both component_style and component_style_rgx. Please choose one or the other.")
+        if parameter_style_rgx is not None and parameter_style is not None:
+            raise ValueError("Cannot specify both parameter_style and parameter_style_rgx. Please choose one or the other.")
+        if component_style_rgx is None and component_style is None :
+            raise ValueError("Component naming style not specified. Use either component_style or component_style_rgx.")
+        if parameter_style_rgx is None and parameter_style is None :
+            raise ValueError("Parameter naming style not specified. Use either parameter_style or parameter_style_rgx.")
+        self.parameterAreas = ["custom", "params"]
+        self.componentAreas = ["root", "children"]
+        self.keysToSkip = ["props", "position", "type", "meta", "propConfig", "scripts"]
+        self.component_style = component_style
+        self.parameter_style = parameter_style
+        self.component_style_rgx = component_style_rgx
+        self.parameter_style_rgx = parameter_style_rgx
+
+        self.component_style_checker = StyleChecker(component_style_rgx) if component_style_rgx is not None else StyleChecker(component_style)
+        self.parameter_style_checker = StyleChecker(parameter_style_rgx) if parameter_style_rgx is not None else StyleChecker(parameter_style)
+
+    
+    def lint_file(self, file_path: str) -> int:
+        errors = {"components": [], "parameters": []}
+
+        with open(file_path, "r") as file:
+            try:
+                data = json.load(file)
+                self.check_component_names(data, errors)
+            except json.JSONDecodeError as e:
+                errors["components"].append(f"Error parsing JSON: {e}")
+
+        self.print_errors(file_path, errors)
+        return len(errors['components']) + len(errors['parameters'])
+
+    def check_parameter_names(self, data, errors: dict, parent_key: str = ""):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                self.check_parameter_names(value, errors, f"{parent_key}.{key}")
+            elif not self.parameter_style_checker.is_correct_style(key) and "props.params" not in parent_key:
+                errors["parameters"].append(f"{parent_key}.{key}" if parent_key else key)
+
+    def check_component_names(self, value, errors: dict, parent_key: str = ""):
+        component_name = value.get("meta", {}).get("name")
+        if component_name == "root":
+            parent_key = component_name
+        elif component_name is not None:
+            parent_key = f"{parent_key}/{component_name}"
+            if not self.component_style_checker.is_correct_style(component_name):
+                errors["components"].append(parent_key)
+
+        for key, value in value.items():
+            if key in self.keysToSkip:
+                continue
+
+            if isinstance(value, dict):
+                if key in self.parameterAreas:
+                    if not parent_key:
+                        parent_key = "view"
+                    self.check_parameter_names(value, errors, f"{parent_key}.{key}")
+                else:
+                    self.check_component_names(value, errors, parent_key)
+            elif isinstance(value, list):
+                parent_of_list = parent_key
+                for item in value:
+                    self.check_component_names(item, errors, parent_key)
+                    parent_key = parent_of_list
+
+    def print_errors(self, file_path: str, errors: dict) -> None:
+        error_logs = []
+        if errors["components"]:
+            if self.component_style_rgx:
+                error_logs.append(f"  Component names should follow pattern '{self.component_style_rgx}':")
+            else:
+                error_logs.append(f"  Component names should be in {self.component_style}:")
+            error_logs.extend([f"    - {error}" for error in errors["components"]])
+        if errors["parameters"]:
+            if self.parameter_style_rgx:
+                error_logs.append(f"  Parameter names should follow pattern '{self.parameter_style_rgx}':")
+            else:
+                error_logs.append(f"  Parameter keys should be in {self.parameter_style}:")
+            error_logs.extend([f"    - {error}" for error in errors["parameters"]])
+
+        if error_logs:
+            print(f"\nError in file: {file_path}")
+            for error_log in error_logs:
+                print(error_log)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--files', default="", help='Space-separated list of ignition files to lint')
+    parser.add_argument('--component-style', help='Naming convention style for components')
+    parser.add_argument('--parameter-style', help='Naming convention style for parameters')
+    parser.add_argument('--component-style-rgx', help='Regex pattern for naming convention style of components')
+    parser.add_argument('--parameter-style-rgx', help='Regex pattern for naming convention style of parameters')
+    args = parser.parse_args()
+
+    input_files = args.files.split()
+    if not input_files:
+        print("No files")
+        sys.exit(0)
+
+    linter = JsonLinter(
+        component_style=args.component_style,
+        parameter_style=args.parameter_style,
+        component_style_rgx=args.component_style_rgx,
+        parameter_style_rgx=args.parameter_style_rgx
+    )
+    number_of_errors = 0
+
+    for file_path in input_files:
+        number_of_errors += linter.lint_file(file_path)
+
+    if number_of_errors:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
