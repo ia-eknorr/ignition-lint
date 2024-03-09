e630b8d7558c8f0ec43caa3971c6d9c33b9d0882
Eric Knorr <50336131+ia-eknorr@users.noreply.github.com>
2024-03-08 22:16:06 -0800

    feat: allow numbers in styles, except as starting char

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -13,27 +13,43 @@ class StyleCheckerTests(unittest.TestCase):
         self.assertTrue(checker.is_correct_style("my_variable"))
         self.assertFalse(checker.is_correct_style("MyVariable"))
         self.assertFalse(checker.is_correct_style("myVariable"))
+        self.assertFalse(checker.is_correct_style("MY_VARIABLE"))
         self.assertTrue(checker.is_correct_style("my_variable_123"))
+        self.assertFalse(checker.is_correct_style("MyVariable123"))
+        self.assertFalse(checker.is_correct_style("myVariable123"))
+        self.assertFalse(checker.is_correct_style("MY_VARIABLE_123"))
 
     def test_is_camel_case(self):
         checker = StyleChecker("camelCase")
         self.assertFalse(checker.is_correct_style("my_variable"))
         self.assertTrue(checker.is_correct_style("myVariable"))
         self.assertFalse(checker.is_correct_style("MyVariable"))
+        self.assertFalse(checker.is_correct_style("MY_VARIABLE"))
         self.assertFalse(checker.is_correct_style("my_variable_123"))
+        self.assertFalse(checker.is_correct_style("MyVariable123"))
+        self.assertTrue(checker.is_correct_style("myVariable123"))
+        self.assertFalse(checker.is_correct_style("MY_VARIABLE_123"))
 
     def test_is_pascal_case(self):
         checker = StyleChecker("PascalCase")
         self.assertFalse(checker.is_correct_style("my_variable"))
         self.assertFalse(checker.is_correct_style("myVariable"))
         self.assertTrue(checker.is_correct_style("MyVariable"))
+        self.assertFalse(checker.is_correct_style("MY_VARIABLE"))
         self.assertFalse(checker.is_correct_style("my_variable_123"))
+        self.assertTrue(checker.is_correct_style("MyVariable123"))
+        self.assertFalse(checker.is_correct_style("myVariable123"))
+        self.assertFalse(checker.is_correct_style("MY_VARIABLE_123"))
 
     def test_is_upper_case(self):
         checker = StyleChecker("UPPER_CASE")
         self.assertFalse(checker.is_correct_style("my_variable"))
         self.assertFalse(checker.is_correct_style("myVariable"))
         self.assertFalse(checker.is_correct_style("MyVariable"))
+        self.assertTrue(checker.is_correct_style("MY_VARIABLE"))
+        self.assertFalse(checker.is_correct_style("my_variable_123"))
+        self.assertFalse(checker.is_correct_style("MyVariable123"))
+        self.assertFalse(checker.is_correct_style("myVariable123"))
         self.assertTrue(checker.is_correct_style("MY_VARIABLE_123"))
 
     def test_is_any(self):
@@ -41,6 +57,10 @@ class StyleCheckerTests(unittest.TestCase):
         self.assertTrue(checker.is_correct_style("my_variable"))
         self.assertTrue(checker.is_correct_style("myVariable"))
         self.assertTrue(checker.is_correct_style("MyVariable"))
+        self.assertTrue(checker.is_correct_style("MY_VARIABLE"))
+        self.assertTrue(checker.is_correct_style("my_variable_123"))
+        self.assertTrue(checker.is_correct_style("MyVariable123"))
+        self.assertTrue(checker.is_correct_style("myVariable123"))                                                 
         self.assertTrue(checker.is_correct_style("MY_VARIABLE_123"))
 
 if __name__ == "__main__":
